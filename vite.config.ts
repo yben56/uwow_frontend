5cbc5d1b5c051ce5a55d54912cfb7beac243accf
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2025-10-20 18:39:33 +0800

    revmoe github preview

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/uwow_frontend/',
   plugins: [
     vue(),
     vueDevTools(),
